e6fe8082837ff2515f30656813b978e34809b965
Ramon <ramm050489@gmail.com>
2021-04-29 22:22:17 -0400

    commit tree b461367ae3d749ea6e56b8e4e6d7ffe787750d7c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hoper';
+
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;  <= first solution offered
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
